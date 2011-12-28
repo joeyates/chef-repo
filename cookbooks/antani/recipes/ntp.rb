144fbfd8dd2bde7c0d77aa4e6ba7f060765fab01
Joe Yates <joe.g.yates@gmail.com>
2011-12-28 18:32:24 +0000

    Whitespace

--- cookbooks/antani/recipes/ntp.rb
+++ cookbooks/antani/recipes/ntp.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,15 +18,14 @@
 #
 
 package "ntp" do
-    action [:install]
-end
- 
-template "/etc/ntp.conf" do
-    source "ntp.conf.erb"
-    variables( :ntp_server => "time.nist.gov" )
-end
- 
-service "ntpd" do
-    action[:enable,:start]
+  action [:install]
 end
 
+template "/etc/ntp.conf" do
+  source "ntp.conf.erb"
+  variables( :ntp_server => "time.nist.gov" )
+end
+
+service "ntpd" do
+  action[:enable,:start]
+end
