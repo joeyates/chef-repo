ee5bb3925fadb15f7843f5d32081826123814c2b
Joe Yates <joe.g.yates@gmail.com>
2011-12-28 17:32:00 +0000

    Added example recipe for ntp

--- cookbooks/foobar/recipes/default.rb
+++ cookbooks/foobar/recipes/default.rb
@@ -16,3 +16,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+package "ntp" do
+    action [:install]
+end
+ 
+template "/etc/ntp.conf" do
+    source "ntp.conf.erb"
+    variables( :ntp_server => "time.nist.gov" )
+end
+ 
+service "ntpd" do
+    action[:enable,:start]
+end
+
