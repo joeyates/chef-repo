e6dd81b1f210c6c16bd11cb337b7e4afaf39b36c
nebirhos <nebirhos@aol.com>
2012-01-28 03:30:55 +0100

    check before install nginx!

--- cookbooks/antani/recipes/nginx.rb
+++ cookbooks/antani/recipes/nginx.rb
@@ -1,35 +1,34 @@
-bash 'compile passenger for Nginx' do
-  not_if { File.directory?( '/opt/nginx' ) }
-  user 'root'
-  code <<-EOH
-  passenger-install-nginx-module --auto-download --prefix=/opt/nginx --auto
-  EOH
-end
+########################
+# nginx
 
-#  http {
-#      ...
-#      passenger_root /usr/lib/ruby/gems/1.9.1/gems/passenger-3.0.11;
-#      passenger_ruby /usr/bin/ruby;
-#      ...
-#  }
+if not File.directory?( '/opt/nginx' )
 
-template '/etc/init.d/nginx' do
-  source 'nginx/nginx.init.erb'
-  mode '0755'
-end
+  bash 'compile passenger for Nginx' do
+    user 'root'
+    code <<-EOH
+    passenger-install-nginx-module --auto-download --prefix=/opt/nginx --auto
+    EOH
+  end
 
-bash 'set init links' do
-  user 'root'
-  code "update-rc.d -f nginx defaults"
-end
+  template '/etc/init.d/nginx' do
+    source 'nginx/nginx.init.erb'
+    mode '0755'
+  end
 
-execute 'setup listening IP' do
-  user 'root'
-  command "sed -ie 's/\\([ ]*listen[ ]*\\)80;/\\1 #{ node[ :nginx ][ :ip ] }:80;/' /opt/nginx/conf/nginx.conf"
-  action :run
-end
+  bash 'set init links' do
+    user 'root'
+    code "update-rc.d -f nginx defaults"
+  end
 
-bash 'start server' do
-  user 'root'
-  code "/etc/init.d/nginx start"
+  execute 'setup listening IP' do
+    user 'root'
+    command "sed -ie 's/\\([ ]*listen[ ]*\\)80;/\\1 #{ node[ :nginx ][ :ip ] }:80;/' /opt/nginx/conf/nginx.conf"
+    action :run
+  end
+
+  bash 'start server' do
+    user 'root'
+    code "/etc/init.d/nginx start"
+  end
+
 end
