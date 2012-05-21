2becec221fe1fe98a0fa32cd92239fcea4f5574c
Joe Yates <joe.g.yates@gmail.com>
2012-05-21 21:31:53 +0100

    Added antani FQDN to hosts

--- cookbooks/antani/attributes/default.rb
+++ cookbooks/antani/attributes/default.rb
@@ -1,4 +1,5 @@
 set[ :antani ][ :hostname ]     = 'antani'
+set[ :antani ][ :fqdn ]         = 'antani.co.uk'
 set[ :antani ][ :ssh ][ :port ] = '41236'
 
 set[ :apache2 ][ :ip ]          = '92.60.123.214'
@@ -9,3 +10,4 @@ set[ :gitosis ][ :home ]        = '/var/git'
 set[ :gitosis ][ :user ]        = 'git'
 
 set[ :webapps ][ :path ]        = '/var/www/apps'
+
